29d010c38864a6647fd872748086bd63ad7904f3
Lukáš Mikuláštík <mikulastik@slajit.cz>
2023-05-03 12:44:34 +0200

    change to localhost

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,6 +1,6 @@
 import {IFlatSort, IFlatWithScore, SortBy, SortDirection} from "../types.ts";
 
-export const LOCAL_IPV4: string = '192.168.100.22'
+export const LOCAL_IPV4: string = '127.0.0.1'
 
 const sortByDirection = (flats: IFlatWithScore[], direction: SortDirection): IFlatWithScore[] => {
     if (direction === SortDirection.DESC) {
